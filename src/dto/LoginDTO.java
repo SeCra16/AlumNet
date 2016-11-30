ed9f8f5a7ff15af7fa59bf8f5f3e0694a4b7a114
Archer <jarcher1200@gmail.com>
2016-11-30 18:35:49 -0500

    Added type to LoginDTO and handled them in the dao. Then added into the persistences.

--- src/dto/LoginDTO.java
+++ src/dto/LoginDTO.java
@@ -2,7 +2,17 @@ package dto;
 
 public class LoginDTO {
 
-	String email = null, password;
+	String email = null;
+	String password;
+	String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 	
 	public String getEmail(){
 		return email;
